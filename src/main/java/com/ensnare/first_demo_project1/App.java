6266558b518bace2fa35bd69296d95cb10649ee0
iamdevopstrainer <iamdevopstrainer@gmail.com>
2022-02-05 07:36:32 +0530

    first project

---
+++ src/main/java/com/ensnare/first_demo_project1/App.java
@@ -0,0 +1,13 @@
+package com.ensnare.first_demo_project1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/ensnare/first_demo_project1/AppTest.java
@@ -0,0 +1,20 @@
+package com.ensnare.first_demo_project1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
